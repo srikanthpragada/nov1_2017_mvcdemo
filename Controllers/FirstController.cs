6ccb69096668dd4940395c4ad135ed0d9a2be712
srikanthpragada <srikanthpragada@gmail.com>
2017-11-28 09:33:32 +0530

    28-NOV

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,39 @@
+﻿using mvcdemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+
+            if (hour < 12)
+                ViewBag.Message = "Good Morning!";
+            else
+               if (hour < 17)
+                 ViewBag.Message = "Good Afternoon";
+               else
+                  ViewBag.Message = "Good Evening";
+
+            ViewBag.Title = "Index";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            Information info = new Information { Topic = "Asp.Net MVC", Author = "Srikanth Pragada" };
+            ViewBag.Title = "About";
+            return View(info);
+        }
+
+        
+    }
+}
---
+++ Models/Information.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvcdemo.Models
+{
+    public class Information
+    {
+        public String Topic { get; set; }
+        public String Author { get; set; }
+    }
+}
